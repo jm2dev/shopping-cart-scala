680b9dcc83a1dc6595578b0c0b430d12738bec95
Jose Miguel <jm@0pt1mates.com>
2016-03-17 17:34:29 +0000

    STEP 2: calculate cost after offers.

--- src/main/scala/ShoppingCart.scala
+++ src/main/scala/ShoppingCart.scala
@@ -6,13 +6,35 @@ class ShoppingCart(input: String) {
   def isValid() = input.matches(regex)
 
   def total() = {
-    val result = input.split(",")
-      .map((fruit:String) => cost(fruit))
-      .foldLeft(0.0)((x: Double, y:Double) => x + y)
+    val result = reducedCost()
 
     isValid() match {
       case true => "£%.2f" format result
       case false => "ERROR: invalid input."
+    }
+  }
+
+  def reducedCost() = {
+    val apples = groupItem("Apple")
+    val oranges = groupItem("Orange")
+
+    reducedCostForApples(apples) + reducedCostForOranges(oranges)
+  }
+
+  private def groupItem(item: String) = input.split(",")
+    .filter(_.equals(item))
+    .length
+
+  private def reducedCostForApples(apples: Int) = Math.floorMod(apples, 2) match {
+    case 0 => apples / 2 * 0.60
+    case _ => (apples / 2 + 1) * 0.60
+  }
+
+  private def reducedCostForOranges(oranges: Int) = {
+    val groups = Math.floorDiv(oranges, 3)
+    Math.floorMod(oranges, 3) match {
+      case 0 => (groups * 2) * 0.25
+      case _ => (oranges - (3 * groups) + (groups * 2)) * 0.25
     }
   }
 
--- src/test/scala/ShoppingCartSpec.scala
+++ src/test/scala/ShoppingCartSpec.scala
@@ -12,6 +12,11 @@ class ShoppingCartSpec extends Specification { def is = s2"""
     cost for an orange is 25p                     $e6
     cost for 'Apple,Orange,Apple,Apple' is 2.05   $e7
     cost for invalid input is error message       $e8
+    buy two apples pay one                        $e9
+    buy three apples pay two                      $e10
+    buy two oranges pay two                       $e11
+    buy three oranges pay two                     $e12
+    buy four oranges pay three                    $e13
   """
 
   def e1 = new ShoppingCart("Apple").isValid() must be equalTo(true)
@@ -20,6 +25,11 @@ class ShoppingCartSpec extends Specification { def is = s2"""
   def e4 = new ShoppingCart("Apple,Orange,Pear").isValid() must be equalTo(false)
   def e5 = new ShoppingCart("Apple").total() must be equalTo("£0.60")
   def e6 = new ShoppingCart("Orange").total() must be equalTo("£0.25")
-  def e7 = new ShoppingCart("Apple,Orange,Apple,Apple").total() must be equalTo("£2.05")
+  def e7 = new ShoppingCart("Apple,Orange,Apple,Apple").total() must be equalTo("£1.45")
   def e8 = new ShoppingCart("Apple,Pear,XXX,Apple").total() must be equalTo("ERROR: invalid input.")
+  def e9 = new ShoppingCart("Apple,Apple").total() must be equalTo("£0.60")
+  def e10 = new ShoppingCart("Apple,Apple,Apple").total() must be equalTo("£1.20")
+  def e11 = new ShoppingCart("Orange,Orange").total() must be equalTo("£0.50")
+  def e12 = new ShoppingCart("Orange,Orange,Orange").total() must be equalTo("£0.50")
+  def e13 = new ShoppingCart("Orange,Orange,Orange,Orange").total() must be equalTo("£0.75")
 }
