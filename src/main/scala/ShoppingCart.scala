586280037bc52c912f6a78184332302dca545185
Jose Miguel <jm@0pt1mates.com>
2016-03-17 17:05:20 +0000

    Calculate cost for shopping cart.

--- src/main/scala/ShoppingCart.scala
+++ src/main/scala/ShoppingCart.scala
@@ -4,4 +4,13 @@ class ShoppingCart(input: String) {
   private val regex = "^[(Apple|Orange),]*(Apple|Orange)$"
 
   def isValid() = input.matches(regex)
+
+  def total() = input.split(",")
+    .map((fruit:String) => cost(fruit))
+    .foldLeft(0.0)((x: Double, y:Double) => x + y)
+
+  private def cost(fruit: String) = fruit match {
+    case "Apple" => 0.60
+    case _ => 0.25
+  }
 }
--- src/test/scala/ShoppingCartSpec.scala
+++ src/test/scala/ShoppingCartSpec.scala
@@ -8,10 +8,16 @@ class ShoppingCartSpec extends Specification { def is = s2"""
     start with 'Orange'                           $e2
     be a comma separed list of apples and oranges $e3
     not contain a different fruit like a 'Pear'   $e4
+    cost for an apple is 60p                      $e5
+    cost for an orange is 25p                     $e6
+    cost for 'Apple,Orange,Apple,Apple' is 2.05   $e7
   """
 
   def e1 = new ShoppingCart("Apple").isValid() must be equalTo(true)
   def e2 = new ShoppingCart("Orange").isValid() must be equalTo(true)
   def e3 = new ShoppingCart("Apple,Orange").isValid() must be equalTo(true)
   def e4 = new ShoppingCart("Apple,Orange,Pear").isValid() must be equalTo(false)
+  def e5 = new ShoppingCart("Apple").total() must be equalTo(0.60)
+  def e6 = new ShoppingCart("Orange").total() must be equalTo(0.25)
+  def e7 = new ShoppingCart("Apple,Orange,Apple,Apple").total() must be equalTo(2.05)
 }
