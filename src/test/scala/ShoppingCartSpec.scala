e9719924e8b428ac20b85625ec862d315ea54080
Jose Miguel <jm@0pt1mates.com>
2016-03-18 14:20:46 +0000

    STEP 1: pretty print cost for shopping cart.

--- src/test/scala/ShoppingCartSpec.scala
+++ src/test/scala/ShoppingCartSpec.scala
@@ -2,7 +2,9 @@ package com.jm2dev.hmrc
 
 import org.specs2.Specification
 
-class ShoppingCartSpec extends Specification { def is = s2"""
+class ShoppingCartSpec extends Specification {
+  def is =
+    s2"""
   Shopping cart input should
     start with 'Apple'                            $e1
     start with 'Orange'                           $e2
@@ -19,17 +21,29 @@ class ShoppingCartSpec extends Specification { def is = s2"""
     buy four oranges pay three                    $e13
   """
 
-  def e1 = new ShoppingCart("Apple").isValid() must be equalTo(true)
-  def e2 = new ShoppingCart("Orange").isValid() must be equalTo(true)
-  def e3 = new ShoppingCart("Apple,Orange").isValid() must be equalTo(true)
-  def e4 = new ShoppingCart("Apple,Orange,Pear").isValid() must be equalTo(false)
-  def e5 = new ShoppingCart("Apple").total() must be equalTo("£0.60")
-  def e6 = new ShoppingCart("Orange").total() must be equalTo("£0.25")
-  def e7 = new ShoppingCart("Apple,Orange,Apple,Apple").total() must be equalTo("£1.45")
-  def e8 = new ShoppingCart("Apple,Pear,XXX,Apple").total() must be equalTo("ERROR: invalid input.")
-  def e9 = new ShoppingCart("Apple,Apple").total() must be equalTo("£0.60")
-  def e10 = new ShoppingCart("Apple,Apple,Apple").total() must be equalTo("£1.20")
-  def e11 = new ShoppingCart("Orange,Orange").total() must be equalTo("£0.50")
-  def e12 = new ShoppingCart("Orange,Orange,Orange").total() must be equalTo("£0.50")
-  def e13 = new ShoppingCart("Orange,Orange,Orange,Orange").total() must be equalTo("£0.75")
+  def e1 = new ShoppingCart("Apple").isValid() must be equalTo (true)
+
+  def e2 = new ShoppingCart("Orange").isValid() must be equalTo (true)
+
+  def e3 = new ShoppingCart("Apple,Orange").isValid() must be equalTo (true)
+
+  def e4 = new ShoppingCart("Apple,Orange,Pear").isValid() must be equalTo (false)
+
+  def e5 = new ShoppingCart("Apple").total() must be equalTo ("£0.60")
+
+  def e6 = new ShoppingCart("Orange").total() must be equalTo ("£0.25")
+
+  def e7 = new ShoppingCart("Apple,Orange,Apple,Apple").total() must be equalTo ("£1.45")
+
+  def e8 = new ShoppingCart("Apple,Pear,XXX,Apple").total() must be equalTo ("ERROR: invalid input.")
+
+  def e9 = new ShoppingCart("Apple,Apple").total() must be equalTo ("£0.60")
+
+  def e10 = new ShoppingCart("Apple,Apple,Apple").total() must be equalTo ("£1.20")
+
+  def e11 = new ShoppingCart("Orange,Orange").total() must be equalTo ("£0.50")
+
+  def e12 = new ShoppingCart("Orange,Orange,Orange").total() must be equalTo ("£0.50")
+
+  def e13 = new ShoppingCart("Orange,Orange,Orange,Orange").total() must be equalTo ("£0.75")
 }
