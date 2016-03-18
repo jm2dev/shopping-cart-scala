99537e0c62fb10161c894b8bc07c1f86115c8acf
Jose Miguel <jm@0pt1mates.com>
2016-03-18 13:40:36 +0000

    Parse input and generate groups of fruits.

--- src/main/scala/Parser.scala
+++ src/main/scala/Parser.scala
@@ -2,11 +2,21 @@ package com.jm2dev.hmrc
 
 class Parser(input: String) {
   private val regex = "^[(Apple|Orange),]*(Apple|Orange)$"
-
   val isValid = input.matches(regex)
 
-  def extract(): Option[String] = isValid match {
-    case true => Some("Apple")
+  def extract() = isValid match {
+    case true => Some(fruits)
     case false => None
+  }
+
+  private val items = input.split(",") groupBy identity values
+
+  private val fruits = items.map(item => createFruit(item))
+
+  private def createFruit(items: Array[String]): Fruit = {
+    items.head match {
+      case "Apple" => new Apple(items.size)
+      case _ => new Orange(items.size) //Not apple then must be orange
+    }
   }
 }
--- src/test/scala/ParserSpec.scala
+++ src/test/scala/ParserSpec.scala
@@ -8,19 +8,25 @@ class ParserSpec extends Specification {
          Input
           should be a comma separated list of 'Apple' or 'Orange' strings     $e1
           shouldn't contain any string different to 'Apple', 'Orange' or ','  $e2
-         Parser should return nothing for invalid input     $e3
-         Parser should return parsed items for valid input  $e4
+         Parser
+          should return nothing for invalid input     $e3
+          should return some items for valid input    $e4
+          should group common items                   $e5
   """
 
   val validInput = "Apple"
   val parserWithValidInput = new Parser(validInput)
-  val parsedItems = "Apple"
+  val parsedItems = Some(new Apple(1))
 
   val invalidInput = "Apple, Pear"
   val parserWithInvalidInput = new Parser(invalidInput)
 
+  val longerInput = "Apple,Orange,Apple"
+  val parserWithLongerInput = new Parser(longerInput)
+
   def e1 = parserWithValidInput.isValid should be equalTo(true)
   def e2 = parserWithInvalidInput.isValid should be equalTo(false)
   def e3 = parserWithInvalidInput.extract() should be equalTo(None)
-  def e4 = parserWithValidInput.extract() should be equalTo(Some(parsedItems))
+  def e4 = parserWithValidInput.extract() should be some
+  def e5 = parserWithLongerInput.extract().get.size should be equalTo(2) //Apple(2), Orange(1)
 }
